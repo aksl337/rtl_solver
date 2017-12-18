ad53d715dd5091f37cd5f961d7a4fc8818dd6fe6
aksl337 <34566812+aksl337@users.noreply.github.com>
2017-12-18 15:25:26 +0530

    final touch

---
+++ README.md
@@ -0,0 +1,4 @@
+#Realtek rtl8723be cheapset wifi reslover
+
+
+read full info https://linuxg33ks.wordpress.com/2017/09/19/ubuntu-14-onwards-rtl8723be-wifi-problem-solved-finally/
---
+++ rtl8723be.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+    echo -e "You must specify an argument as follow----> \n enter 1 for all(Installation+check)\n or \n 2 to only check wifi link up or not."
+    exit 100
+fi
+case $1 in
+    1)
+        cd '/root/rtl/rtlwifi_new-master';  #cd to extracted directory of drivers.
+        make clean;  # if you tried installing same drivers already then "clean" comes handy.
+        make install;
+        modprobe -rv rtl8723be;
+        modprobe -v rtl8723be ;
+        sleep 4
+        output=$(iw dev wlo1 scan)
+        if [[ $output ]]; then
+             echo "Success............."
+             read -p "press enter to exit"
+        else
+           echo "Command failed,Sir.what to do now?.. want reboot? press Y..........."
+            read Varcom
+            if [ [$Varcom == Y] ]; then
+                reboot;
+            else
+                echo 'Exiting...'
+                exit 0 ;
+            fi
+        fi
+        ;;
+    2)
+        output=$(iw dev wlo1 scan)
+        if [[ $output ]]; then
+             echo "Success............."
+             read -p "press enter to exit"
+        else
+           echo "Command failed,Sir.what to do now?.. want reboot? press Y..........."
+            read Varcom
+            if [ [$Varcom == Y] ]; then
+                reboot;
+            else
+                echo 'Exiting...'
+                exit 0 ;
+            fi
+        fi
+        ;;
+    *)
+        echo -e "$1 is not recognized !!! try only 1 or 2 option.\n"
+        echo -e "You must specify an argument as follow----> \n enter 1 for all(Installation+check)\n or \n 2 to only check wifi link up or not."
+esac
